62fe359cfd13159fa99522932b1c380b11de70a0
weichichou <weichi.cc@gmail.com>
2020-06-22 21:50:31 +0200

    Flip cards animation

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,10 +3,21 @@ import { CardData } from "../model";
 type Props = CardData;
 
 export default class Card extends React.Component<Props> {
+  state = {
+    flipped: false,
+  };
   render() {
     return (
-      <div className="card-div">
-        <img src={this.props.img} />
+      <div
+        onClick={() => this.setState({ flipped: !this.state.flipped })}
+        className="scene"
+      >
+        <div className={`card ${this.state.flipped ? "flipped" : ""}`}>
+          <div className="card_face front"></div>
+          <div className="card_face back">
+            <img src={this.props.img} />
+          </div>
+        </div>
       </div>
     );
   }
